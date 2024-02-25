4a5935a3dbd4086a4f3cd2d8abfbe6140dfcaf1f
MCMepitap <epitap8@gmail.com>
2024-02-25 22:48:04 +0900

    change Hologram

--- src/main/java/net/epitap/degeneracycraft/datagen/DCBlockStateProvider.java
+++ src/main/java/net/epitap/degeneracycraft/datagen/DCBlockStateProvider.java
@@ -38,8 +38,7 @@ public class DCBlockStateProvider extends BlockStateProvider {
         simpleBlock(DCBlocks.LOW_STRENGTH_MULTIBOOT_STRUCTURE_FRAME_BLOCK.get());
         simpleBlock(DCBlocks.LOW_STRENGTH_MULTIBOOT_STRUCTURE_GLASS_BLOCK.get());
 
-
-        simpleBlock(DCHoloBlocks.BASIC_STRENGTH_MULTIBLOCK_BASE_FRAME_HOLO_BLOCK.get());
+//        simpleBlock(DCHoloBlocks.BASIC_STRENGTH_MULTIBLOCK_BASE_FRAME_HOLO_BLOCK.get());
         simpleBlock(DCHoloBlocks.BASIC_STRENGTH_MULTIBLOCK_MACHINE_FRAME_HOLO_BLOCK.get());
         simpleBlock(DCHoloBlocks.BASIC_STRENGTH_MULTIBLOCK_STRUCTURE_FRAME_HOLO_BLOCK.get());
         simpleBlock(DCHoloBlocks.BASIC_STRENGTH_MULTIBLOCK_STRUCTURE_GLASS_HOLO_BLOCK.get());

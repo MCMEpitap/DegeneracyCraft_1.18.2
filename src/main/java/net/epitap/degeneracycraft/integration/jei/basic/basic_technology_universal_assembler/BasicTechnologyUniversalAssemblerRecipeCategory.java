c03eedfbac38bfef051be07c3fe7fda90ed2a7aa
MCMepitap <epitap8@gmail.com>
2024-06-29 00:43:58 +0900

    add BasicTechnologyUniversalAssemblerBlock

--- src/main/java/net/epitap/degeneracycraft/integration/jei/basic/basic_technology_universal_assembler/BasicTechnologyUniversalAssemblerRecipeCategory.java
+++ src/main/java/net/epitap/degeneracycraft/integration/jei/basic/basic_technology_universal_assembler/BasicTechnologyUniversalAssemblerRecipeCategory.java
@@ -1,14 +1,18 @@
 package net.epitap.degeneracycraft.integration.jei.basic.basic_technology_universal_assembler;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.epitap.degeneracycraft.Degeneracycraft;
 import net.epitap.degeneracycraft.blocks.base.DCBlocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -21,19 +25,27 @@ public class BasicTechnologyUniversalAssemblerRecipeCategory implements IRecipeC
     public final static ResourceLocation UID = new ResourceLocation(Degeneracycraft.MOD_ID, "basic_technology_universal_assembler_recipe");
     public final static ResourceLocation TEXTURE =
             new ResourceLocation(Degeneracycraft.MOD_ID, "textures/gui/basic_technology_universal_assembler_gui.png");
-
-
     private final IDrawable background;
     private final IDrawable icon;
 
     public BasicTechnologyUniversalAssemblerRecipeCategory(IGuiHelper helper) {
-        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 130);
-        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM, new ItemStack(DCBlocks.BASIC_TECHNOLOGY_UNIVERSAL_ASSEMBLER_BLOCK.get()));
+        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 151);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM, new ItemStack(DCBlocks.REDSTONE_POWERED_MACHINE_PART_MANUFACTURE_MACHINE_BLOCK.get()));
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
+    }
+
+    @Override
+    public Class<? extends BasicTechnologyUniversalAssemblerRecipe> getRecipeClass() {
+        return BasicTechnologyUniversalAssemblerRecipe.class;
     }
 
     @Override
     public Component getTitle() {
-        return new TranslatableComponent("jei.basic_technology_universal_assembler_recipe");
+        return new TranslatableComponent("jei.degeneracycraft_redstone_powered_machine_part_manufacture_machine");
     }
 
     @Override
@@ -47,29 +59,54 @@ public class BasicTechnologyUniversalAssemblerRecipeCategory implements IRecipeC
     }
 
     @Override
-    public ResourceLocation getUid() {
-        return UID;
+    public void draw(BasicTechnologyUniversalAssemblerRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        drawPhase(stack);
+        drawRequiredEnergy(recipe, stack);
+        drawRequiredEnergyUsage(recipe, stack);
+        drawRequiredTime(recipe, stack);
     }
 
-    @Override
-    public Class<? extends BasicTechnologyUniversalAssemblerRecipe> getRecipeClass() {
-        return BasicTechnologyUniversalAssemblerRecipe.class;
+    protected void drawPhase(PoseStack poseStack) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        fontRenderer.draw(poseStack, new TranslatableComponent("screen." + "degeneracycraft" + ".phase0"), 15, 67, 0xFFFFFF);
     }
 
-//    @Override
-//    public Class<? extends Pulverization> getRecipeClass() {
-//        return Pulverization.class;
-//    }
+    protected void drawRequiredEnergy(BasicTechnologyUniversalAssemblerRecipe recipe, PoseStack poseStack) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        fontRenderer.draw(poseStack, new TranslatableComponent("tooltip.degeneracycraft.requiredenergy."), 17, 87, 0xFFFFFF);
+        fontRenderer.draw(poseStack, (recipe.getRequiredEnergy() + " FE"), 17, 97, 0xFFFFFF);
+    }
+
+    protected void drawRequiredEnergyUsage(BasicTechnologyUniversalAssemblerRecipe recipe, PoseStack poseStack) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        fontRenderer.draw(poseStack, new TranslatableComponent("tooltip.degeneracycraft.requiredenergyusage."), 17, 107, 0xFFFFFF);
+        fontRenderer.draw(poseStack, (recipe.getRequiredEnergy() / recipe.getRequiredTime() / 20 + " FE/t"), 17, 117, 0xFFFFFF);
+
+    }
+
+    protected void drawRequiredTime(BasicTechnologyUniversalAssemblerRecipe recipe, PoseStack poseStack) {
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        fontRenderer.draw(poseStack, new TranslatableComponent("tooltip.degeneracycraft.requiredtime."), 17, 127, 0xFFFFFF);
+        fontRenderer.draw(poseStack, recipe.getRequiredTime() * 20 + " tick" + " " + "(" + recipe.getRequiredTime() + " Sec" + ")", 17, 137, 0xFFFFFF);
+    }
+
 
     @Override
     public void setRecipe(@Nonnull IRecipeLayoutBuilder builder, @Nonnull BasicTechnologyUniversalAssemblerRecipe recipe, @Nonnull IFocusGroup focusGroup) {
-
-//            builder.addSlot(RecipeIngredientRole.INPUT, 44, 6).addItemStack(new ItemStack(Items.IRON_INGOT));
-//            builder.addSlot(RecipeIngredientRole.INPUT, 62, 6).addItemStack(new ItemStack(Items.IRON_INGOT));
-//            builder.addSlot(RecipeIngredientRole.OUTPUT, 116, 6).addItemStack(new ItemStack(DCItems.IRON_BOLT.get()));
-        builder.addSlot(RecipeIngredientRole.INPUT, 44, 6).addIngredients(recipe.getIngredients().get(0));
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 116, 30).addItemStack(recipe.getResultItem());
+        builder.addSlot(RecipeIngredientRole.INPUT, 8, 7).addItemStack(recipe.getInput0Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 26, 7).addItemStack(recipe.getInput1Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 44, 7).addItemStack(recipe.getInput2Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 8, 25).addItemStack(recipe.getInput3Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 26, 25).addItemStack(recipe.getInput4Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 44, 25).addItemStack(recipe.getInput5Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 8, 43).addItemStack(recipe.getInput6Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 26, 43).addItemStack(recipe.getInput7Item());
+        builder.addSlot(RecipeIngredientRole.INPUT, 44, 43).addItemStack(recipe.getInput8Item());
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 116, 25).addItemStack(recipe.getOutput0Item());
     }
-
-    }
+}
 

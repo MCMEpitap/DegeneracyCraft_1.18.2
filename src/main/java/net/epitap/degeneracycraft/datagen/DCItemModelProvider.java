b340f11ed21fd58f9fa197aba4fba3b18c76026d
MCMepitap <epitap8@gmail.com>
2024-08-18 01:50:21 +0900

    add ability block

--- src/main/java/net/epitap/degeneracycraft/datagen/DCItemModelProvider.java
+++ src/main/java/net/epitap/degeneracycraft/datagen/DCItemModelProvider.java
@@ -690,19 +690,27 @@ public class DCItemModelProvider extends ItemModelProvider {
                 modLoc("block/basic_endurance_high_temperature_combustion_chamber_holo_block"));
         withExistingParent(DCBlocks.BASIC_POWER_THERMAL_GENERATOR_BUS_BLOCK.getId().getPath(),
                 modLoc("block/basic_power_composite_structure_type_thermal_generator_bus_block"));
+        withExistingParent(DCBlocks.BASIC_POWER_THERMAL_GENERATOR_BUS_HOLO_BLOCK.getId().getPath(),
+                modLoc("block/basic_power_composite_structure_type_thermal_generator_bus_holo_block"));
         withExistingParent(DCBlocks.BASIC_POWER_THERMAL_GENERATOR_PORT_BLOCK.getId().getPath(),
                 modLoc("block/basic_power_composite_structure_type_thermal_generator_port_block"));
+        withExistingParent(DCBlocks.BASIC_POWER_THERMAL_GENERATOR_PORT_HOLO_BLOCK.getId().getPath(),
+                modLoc("block/basic_power_composite_structure_type_thermal_generator_port_holo_block"));
 
         withExistingParent(DCBlocks.BASIC_TECHNOLOGY_MACHINE_MANUFACTURER_BLOCK.getId().getPath(),
                 modLoc("block/basic_technology_machine_manufacturer_block"));
         withExistingParent(DCBlocks.BASIC_EFFICIENCY_MACHINE_PROCESSING_CHAMBER_BLOCK.getId().getPath(),
                 modLoc("block/basic_efficiency_machine_processing_chamber_block"));
-
+        withExistingParent(DCBlocks.BASIC_EFFICIENCY_MACHINE_PROCESSING_CHAMBER_HOLO_BLOCK.getId().getPath(),
+                modLoc("block/basic_efficiency_machine_processing_chamber_holo_block"));
         withExistingParent(DCBlocks.BASIC_TECHNOLOGY_MACHINE_MANUFACTURER_BUS_BLOCK.getId().getPath(),
                 modLoc("block/basic_technology_machine_manufacturer_bus_block"));
+        withExistingParent(DCBlocks.BASIC_TECHNOLOGY_MACHINE_MANUFACTURER_BUS_HOLO_BLOCK.getId().getPath(),
+                modLoc("block/basic_technology_machine_manufacturer_bus_holo_block"));
         withExistingParent(DCBlocks.BASIC_TECHNOLOGY_MACHINE_MANUFACTURER_PORT_BLOCK.getId().getPath(),
                 modLoc("block/basic_technology_machine_manufacturer_port_block"));
-
+        withExistingParent(DCBlocks.BASIC_TECHNOLOGY_MACHINE_MANUFACTURER_PORT_HOLO_BLOCK.getId().getPath(),
+                modLoc("block/basic_technology_machine_manufacturer_port_holo_block"));
 
         withExistingParent(DCBlocks.BASIC_TECHNOLOGY_UNIVERSAL_ASSEMBLER_BLOCK.getId().getPath(),
                 modLoc("block/basic_technology_universal_assembler_block"));

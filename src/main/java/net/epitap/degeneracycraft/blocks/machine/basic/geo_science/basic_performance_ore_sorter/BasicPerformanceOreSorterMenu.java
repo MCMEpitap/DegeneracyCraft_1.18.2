b78f56fb097e82caab9512dc885335fddf7b8c02
MCMEpitap <epitap8@gmail.com>
2026-04-05 13:51:18 +0900

    change BasicPerformanceOreSorter

--- src/main/java/net/epitap/degeneracycraft/blocks/machine/basic/geo_science/basic_performance_ore_sorter/BasicPerformanceOreSorterMenu.java
+++ src/main/java/net/epitap/degeneracycraft/blocks/machine/basic/geo_science/basic_performance_ore_sorter/BasicPerformanceOreSorterMenu.java
@@ -27,7 +27,7 @@ public class BasicPerformanceOreSorterMenu extends AbstractContainerMenu {
     public final ContainerData data;
 
     public BasicPerformanceOreSorterMenu(int id, Inventory inv, FriendlyByteBuf extraData) {
-        this(id, inv, inv.player.level.getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(5));
+        this(id, inv, inv.player.level.getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(6));
     }
 
     public BasicPerformanceOreSorterMenu(int id, Inventory inv, BlockEntity entity, ContainerData data) {
@@ -65,6 +65,10 @@ public class BasicPerformanceOreSorterMenu extends AbstractContainerMenu {
 
     public int getMultiblockLevel() {
         return data.get(4);
+    }
+
+    public boolean isInputLocked() {
+        return data.get(5) != 0;
     }
 
     public DCIEnergyStorageFloat getEnergy() {
--- src/main/java/net/epitap/degeneracycraft/blocks/machine/basic/geo_science/basic_performance_ore_sorter/BasicPerformanceOreSorterStructure.java
+++ src/main/java/net/epitap/degeneracycraft/blocks/machine/basic/geo_science/basic_performance_ore_sorter/BasicPerformanceOreSorterStructure.java
@@ -24,34 +24,34 @@ public class BasicPerformanceOreSorterStructure {
             {
                     {"F", "F", "F"},
                     {"F", "F", "F"},
-                    {"E", "B", "E"}
+                    {"1", "F", "1"}
             },
             {
                     {"F", "G", "F"},
                     {"G", " ", "G"},
-                    {"P", " ", "P"}
+                    {"F", " ", "F"}
             },
             {
                     {"F", "F", "F"},
                     {"F", "F", "F"},
-                    {"S", "F", "S"}
+                    {"3", "F", "4"}
             }
     };
     public static String[][][] structure1 = new String[][][]{
             {
                     {"F", "F", "F"},
                     {"F", "F", "F"},
-                    {"E", "B", "E"}
+                    {"1", "F", "1"}
             },
             {
                     {"F", "G", "F"},
                     {"G", "O", "G"},
-                    {"P", " ", "P"}
+                    {"F", " ", "F"}
             },
             {
                     {"F", "F", "F"},
                     {"F", "F", "F"},
-                    {"S", "F", "S"}
+                    {"3", "F", "4"}
             }
     };
 
@@ -110,11 +110,11 @@ public class BasicPerformanceOreSorterStructure {
         Map<String, Predicate<BlockState>> blockConditions = new HashMap<>();
         blockConditions.put("F", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_STRUCTURE_FRAME_BLOCK.get()));
         blockConditions.put("G", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_STRUCTURE_GLASS_BLOCK.get()));
-        blockConditions.put("P", blockState -> blockState.is(DCBlocks.BASIC_PERFORMANCE_ORE_SORTER_PORT_BLOCK.get()));
-        blockConditions.put("S", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ITEM_STORAGE_BLOCK.get()));
-        blockConditions.put("B", blockState -> blockState.is(DCBlocks.BASIC_PERFORMANCE_ORE_SORTER_BUS_BLOCK.get()));
-        blockConditions.put("E", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ENERGY_STORAGE_BLOCK.get()));
         blockConditions.put("O", blockState -> blockState.is(DCBlocks.BASIC_EFFICIENCY_ORE_SORTING_FILTER_BLOCK.get()));
+        blockConditions.put("1", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ENERGY_INPUT_BUS_BLOCK.get()));
+        blockConditions.put("2", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ENERGY_OUTPUT_BUS_BLOCK.get()));
+        blockConditions.put("3", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ITEM_INPUT_PORT_BLOCK.get()));
+        blockConditions.put("4", blockState -> blockState.is(DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ITEM_OUTPUT_PORT_BLOCK.get()));
         return blockConditions;
     }
 
@@ -157,11 +157,11 @@ public class BasicPerformanceOreSorterStructure {
         Map<String, Supplier<BlockState>> blockSuppliers = new HashMap<>();
         blockSuppliers.put("F", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_STRUCTURE_FRAME_HOLO_BLOCK.get().defaultBlockState());
         blockSuppliers.put("G", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_STRUCTURE_GLASS_HOLO_BLOCK.get().defaultBlockState());
-        blockSuppliers.put("P", () -> DCBlocks.BASIC_PERFORMANCE_ORE_SORTER_PORT_HOLO_BLOCK.get().defaultBlockState());
-        blockSuppliers.put("S", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ITEM_STORAGE_HOLO_BLOCK.get().defaultBlockState());
-        blockSuppliers.put("B", () -> DCBlocks.BASIC_PERFORMANCE_ORE_SORTER_BUS_HOLO_BLOCK.get().defaultBlockState());
-        blockSuppliers.put("E", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ENERGY_STORAGE_HOLO_BLOCK.get().defaultBlockState());
         blockSuppliers.put("O", () -> DCBlocks.BASIC_EFFICIENCY_ORE_SORTING_FILTER_BLOCK.get().defaultBlockState());
+        blockSuppliers.put("1", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ENERGY_INPUT_BUS_HOLO_BLOCK.get().defaultBlockState());
+        blockSuppliers.put("2", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ENERGY_OUTPUT_BUS_HOLO_BLOCK.get().defaultBlockState());
+        blockSuppliers.put("3", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ITEM_INPUT_PORT_HOLO_BLOCK.get().defaultBlockState());
+        blockSuppliers.put("4", () -> DCBlocks.BASIC_STRENGTH_GEO_SCIENCE_MULTIBLOCK_ITEM_OUTPUT_PORT_HOLO_BLOCK.get().defaultBlockState());
         return blockSuppliers;
     }
 
@@ -218,7 +218,7 @@ public class BasicPerformanceOreSorterStructure {
         return false;
     }
 
-    private static BlockPos getRelativePos(BlockPos basePos, int x, int y, int z, Direction facing) {
+    public static BlockPos getRelativePos(BlockPos basePos, int x, int y, int z, Direction facing) {
         return switch (facing) {
             case NORTH -> basePos.relative(Direction.WEST, x).relative(Direction.UP, y).relative(Direction.NORTH, z);
             case SOUTH -> basePos.relative(Direction.EAST, x).relative(Direction.UP, y).relative(Direction.SOUTH, z);
